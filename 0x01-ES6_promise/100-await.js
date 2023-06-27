49c8859d7758215d2d677fe2c881d201861ea8f2
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-06-27 14:30:04 +0100

    Fix 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const uploadBody = await uploadPhoto();
-  const userBody = await createUser();
+  try {
+    const uploadBody = await uploadPhoto();
+    const userBody = await createUser();
 
-  if (!uploadBody || !userBody) {
-    return { photo: null, user: null };
+    if (!uploadBody || !userBody) {
+      return { photo: null, user: null };
+    }
+    return { photo: uploadBody, user: userBody };
+  } catch (err) {
+    if (err) {
+      return { photo: null, user: null };
+    }
   }
-  return { photo: uploadBody, user: userBody };
 }
