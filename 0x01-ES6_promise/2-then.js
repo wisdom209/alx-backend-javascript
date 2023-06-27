8b71554119824038d7d11fe539cb5f206951e001
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-06-27 21:44:36 +0100

    fix 2-then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,6 +5,6 @@ export default function handleResponseFromAPI(promise) {
       return { status: 200, body: 'success' };
     })
     .catch(() => {
-      Error();
+      return Error();
     });
 }
