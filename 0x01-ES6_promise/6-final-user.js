929dd918877d60a141b84445e0b91c52ac0f569d
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-06-27 12:36:01 +0100

    settled promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstname, lastname, filename) {
+  const signup = signUpUser(firstname, lastname);
+  const photoUpload = uploadPhoto(filename);
+
+  return Promise.allSettled([signup, photoUpload]);
+}
