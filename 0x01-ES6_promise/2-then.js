1efe9cb164daa3dff42da87b7dbef437d49c7df6
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-06-27 21:44:23 +0100

    fix 2-then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  try {
-    promise
-      .then(() => {
-        console.log('Got a response from the API');
-        return { status: 200, body: 'success' };
-      })
-      .catch(() => Error());
-  } catch (error) {
-    if (error) {
-      return Error();
-    }
-  }
-  return null;
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      Error();
+    });
 }
