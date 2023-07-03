fd6e7143ad480eed6fc1c93a6acbdfc8dcdfd16b
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-03 12:14:44 +0100

    Added 5-typed_arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,6 +2,9 @@ export default function createIntTypedArray(length, position, value) {
   const modArr = new ArrayBuffer(length);
 
   const arr = new DataView(modArr);
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
   arr.setUint8(position, value);
   return arr;
 }
