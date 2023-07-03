c22fef6acdb367b0f96fb92a2c896a566bbcde89
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-03 19:35:19 +0100

    Modified 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,18 +2,11 @@ export default function cleanSet(aSet, startString) {
   const stringArr = [];
   const removeLen = startString.length;
 
-  if (startString === '') {
-    return '';
-  }
-
-  for (const i of aSet) {
-    if (i.length > removeLen) {
-      const toCheck = i.slice(0, removeLen);
-      if (toCheck === startString) {
-        stringArr.push(i.slice(removeLen, i.length));
-      }
+  aSet.forEach((element) => {
+    if (element.startsWith(startString)) {
+      stringArr.push(element.slice(removeLen));
     }
-  }
+  });
 
   return stringArr.join('-');
 }
