4c56014b5205f0fadd971e72ca6aa8dba5e23375
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-22 13:00:52 +0100

    update

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,5 @@
 class AppController {
-  static getHomepage (request, response) {
+  static getHomepage(request, response) {
     response.statusCode = 200;
     response.end('Hello Holberton School!');
   }
--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,39 +1,39 @@
-import readDatabase from '../utils.js';
+import readDatabase from '../utils';
 
 class StudentsController {
-  static getAllStudents (request, response) {
+  static getAllStudents(request, response) {
     const databasePath = process.argv[2];
-    readDatabase(databasePath).then(res => {
+    readDatabase(databasePath).then((res) => {
       response.statusCode = 200;
       const title = 'This is the list of our students\n';
       const cs = `Number of students in CS: ${res.CS.length}. List: ${res.CS.join(', ')}\n`;
       const swe = `Number of students in SWE: ${res.SWE.length}. List: ${res.SWE.join(', ')}`;
       const resMsg = title + cs + swe;
       response.end(resMsg);
-    }).catch(e => {
+    }).catch(() => {
       response.statusCode = 500;
       response.end('Cannot load the database');
     });
   }
 
-  static getAllStudentsByMajor (request, response) {
+  static getAllStudentsByMajor(request, response) {
     if (request.params.major === 'CS') {
       const databasePath = process.argv[2];
-      readDatabase(databasePath).then(res => {
+      readDatabase(databasePath).then((res) => {
         response.statusCode = 200;
         const resMsg = `List: ${res.CS.join(', ')}`;
         response.end(resMsg);
-      }).catch(e => {
+      }).catch(() => {
         response.statusCode = 500;
         response.end('Cannot load the database');
       });
     } else if (request.params.major === 'SWE') {
       const databasePath = process.argv[2];
-      readDatabase(databasePath).then(res => {
+      readDatabase(databasePath).then((res) => {
         response.statusCode = 200;
         const resMsg = `List: ${res.SWE.join(', ')}`;
         response.end(resMsg);
-      }).catch(e => {
+      }).catch(() => {
         response.statusCode = 500;
         response.end('Cannot load the database');
       });
