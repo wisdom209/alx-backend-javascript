95c843f7f77ada38f3b652216fa27f14c3223143
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-22 13:04:33 +0100

    update

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,6 +2,7 @@ import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', AppController.getHomepage);
