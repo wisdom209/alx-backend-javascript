75591e486a8576b1a9897d4d6895b98bf0a8b220
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-22 11:41:26 +0100

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\n`);
+  const input = data.toString();
+  process.stdout.write(`Your name is: ${input}`);
   if (process.stdin.isTTY) {
     process.exit();
+  } else {
+    process.stdin.on('close', () => {
+      process.stdout.write('This important software is now closing\n');
+    });
   }
-});
-
-process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing\n');
 });
