8e294a5b801530459553cbf6b4e3f420ce4dc3e0
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-22 12:39:17 +0100

    update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,13 +2,14 @@ const http = require('http');
 const countStudents = require('./3-read_file_async.js');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   }
   if (req.url === '/students') {
     const path = process.argv[2];
     countStudents(path).then((resp) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       const response = `This is the list of our students\n${resp}`;
       res.end(response);
     }).catch(() => {
