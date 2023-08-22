f782e7564af1f317986b6efaa794ef12d74bd6a9
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-22 11:48:38 +0100

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  fs.readFile(path, (err, data) => {
+  fs.readFile(path, (err, retData) => {
     if (err) {
       throw err('Cannot load the database');
     }
 
-    data = data.toString().split('\n');
+    const data = retData.toString().split('\n');
 
-    const allStudents = data.filter((v, i, a) => {
-      return i !== 0 && v.length !== 0;
-    });
+    const allStudents = data.filter((v, i) => i !== 0 && v.length !== 0);
 
-    const csStudents = allStudents.filter(v => v.toString().endsWith('CS')).map(v => v.split(',')[0]);
-    const sweStudents = allStudents.filter(v => v.toString().endsWith('SWE')).map(v => v.split(',')[0]);
+    const csStudents = allStudents.filter((v) => v.toString().endsWith('CS')).map((v) => v.split(',')[0]);
+    const sweStudents = allStudents.filter((v) => v.toString().endsWith('SWE')).map((v) => v.split(',')[0]);
 
     const NUMBER_OF_STUDENTS = allStudents.length;
     const numCsStudents = csStudents.length;
